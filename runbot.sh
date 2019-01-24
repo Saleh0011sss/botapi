3388cc4299b0be93895535f0b5eabf0c391b0b72
SaleH [UnixIR] <salehfffaaa@gmail.com>
2019-01-24 18:59:10 +0330

    Update runbot.sh

--- runbot.sh
+++ runbot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #########################################################################
-#Developed by Electrovirus                                              #
+#                                                                       #
 #You can edit the path to select the correct .py file for run the script#
 #########################################################################
 
